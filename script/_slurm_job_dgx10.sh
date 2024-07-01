4549f583764167887adf1e1eff96824203abd735
Herbert Ullrich <ullriher@fel.cvut.cz>
2024-07-01 13:48:44 +0200

    run_dev complete

--- script/_slurm_job_dgx10.sh
+++ script/_slurm_job_dgx10.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-#SBATCH --partition amdgpufast
+#SBATCH --partition interactive
 #SBATCH --nodes 1
 #SBATCH --ntasks-per-node 1
-#SBATCH --mem-per-cpu 96G
+#SBATCH --mem-per-cpu 128G
 #SBATCH --gres gpu:1
-#SBATCH --time 4:00:00
+#SBATCH --time 72:00:00
 #SBATCH --job-name averitec
 #SBATCH --output logs/jupyter.%j.out
-
+unset LMOD_ROOT; unset MODULESHOME; unset LMOD_PKG; unset LMOD_CMD; unset LMOD_DIR; unset FPATH; unset __LMOD_REF_COUNT_MODULEPATH; unset __LMOD_REF_COUNT__LMFILES_; unset _LMFILES_; unset _ModuleTable001_; unset _ModuleTable002_
+source /etc/profile.d/lmod.sh
 ml Python/3.10.4-GCCcore-11.3.0-bare
 
 # Replace with your own virtual environment
