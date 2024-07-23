31bd174f53dbb38192f1486144ceb2e23c9988b4
Herbert Ullrich <ullriher@fel.cvut.cz>
2024-07-23 15:25:41 +0200

    almost done polishing

--- src/averitec.py
+++ src/averitec.py
@@ -1,4 +1,5 @@
 import numpy as np
+import json
 from dataclasses import dataclass, field
 from typing import Any, Callable, List, Tuple, Union, Dict
 from utils.chat import SimpleJSONChat
@@ -6,6 +7,7 @@ from langchain_core.documents import Document
 from langchain_core.embeddings import Embeddings
 from langchain_community.vectorstores import FAISS
 from anthropic import AnthropicVertex
+from scipy.special import softmax
 
 label2id = {"Refuted": 0, "Supported": 1, "Not Enough Evidence": 2, "Conflicting Evidence/Cherrypicking": 3}
 id2label = {v: k for k, v in label2id.items()}
@@ -37,7 +39,6 @@ class Evidence:
 @dataclass
 class EvidenceGenerationResult:
     evidences: List[Evidence] = field(default_factory=list)
-    proposed_label: str = None
     metadata: Dict[str, Any] = None
 
     def __iter__(self):
@@ -147,6 +148,75 @@ class SimpleFaissRetriever(Retriever):
 
 
 class EvidenceGenerator:
+    @classmethod
+    def parse_label(cls, label: str) -> str:
+        if "sup" in label.lower():
+            return "Supported"
+        elif "ref" in label.lower():
+            return "Refuted"
+        elif "conf" in label.lower() or "cherr" in label.lower():
+            return "Conflicting Evidence/Cherrypicking"
+        elif "not" in label.lower():
+            return "Not Enough Evidence"
+        return "Refuted"
+
+    @classmethod
+    def parse_likert(cls, likert_string: str) -> float:
+        if "1" in likert_string or ("strong" in likert_string and "disagree" in likert_string):
+            return -2
+        if "5" in likert_string or ("strong" in likert_string and "agree" in likert_string):
+            return 2
+        if "2" in likert_string or ("disagree" in likert_string):
+            return -1
+        if "3" in likert_string or "neutral" in likert_string:
+            return 0
+        if "4" in likert_string or ("agree" in likert_string):
+            return 1
+        return 0
+
+    @classmethod
+    def parse_label_probabilities(cls, data: dict) -> np.ndarray:
+        result = np.zeros(4)
+        for label, likert in data.items():
+            result[cls.parse_label(label2id[label])] = cls.parse_likert(likert)
+        return softmax(result)
+
+    @classmethod
+    def parse_json(cls, message):
+        try:
+            result = message
+            # trim message before first ```
+            if "```json" in message:
+                message = message.split("```json")[1]
+            if "```" in message:
+                message = message.split("```")[0]
+            result = message.replace("```json", "").replace("```", "")
+            return json.loads(result)
+        except:
+            print("Error parsing JSON for EvidenceGenerator.\n", message)
+            return []
+
+    @classmethod
+    def parse_evidence(cls, input_data, retrieval_result) -> List[Evidence]:
+        result = []
+        for e in input_data:
+            evidence = Evidence(question=e.get("question", None), answer=e.get("answer", None))
+            try:
+                id = int(str(e["source"]).split(",")[0]) - 1
+                evidence.url = retrieval_result[id].metadata["url"]
+                evidence.scraped_text = "\n".join(
+                    [
+                        retrieval_result[id].metadata["context_before"],
+                        retrieval_result[id].page_content,
+                        retrieval_result[id].metadata["context_after"],
+                    ]
+                )
+            except:
+                evidence.url = None
+                evidence.scraped_text = None
+            result.append(evidence)
+        return result
+
     def __call__(
         self, datapoint: Datapoint, retrieval_result: RetrievalResult, *args, **kwargs
     ) -> EvidenceGenerationResult:
@@ -156,20 +226,43 @@ class EvidenceGenerator:
 class GptEvidenceGenerator(EvidenceGenerator):
     def __init__(self, client: SimpleJSONChat = None, model="gpt-4o"):
         if client is None:
-            self.client = SimpleJSONChat(model=model)
+            self.client = SimpleJSONChat(model=model, parse_output=False)
+
+    def format_system_prompt(self, retrieval_result: RetrievalResult) -> str:
+        result = "You are a professional fact checker, formulate up to 10 questions that cover all the facts needed to validate whether the factual statement (in User message) is true, false, uncertain or a matter of opinion.\nAfter formulating Your questions and their answers using the provided sources, You evaluate the possible veracity verdicts (Supported claim, Refuted claim, Not enough evidence, or Conflicting evidence/Cherrypicking) given your claim and evidence on a Likert scale (1 - Strongly disagree, 2 - Disagree, 3 - Neutral, 4 - Agree, 5 - Strongly agree).\nThe facts must be coming from these sources, please refer them using assigned IDs:"
+        for i, e in enumerate(retrieval_result):
+            result += f"\n---\n## Source ID: {i+1} ({e.metadata['url']})\n"
+            result += "\n".join([e.metadata["context_before"], e.page_content, e.metadata["context_after"]])
+        result += """\n---\n## Output formatting\nPlease, you MUST only print the output in the following output format:
+```json
+{
+    "questions":
+        [
+            {"question": "<Your first question>", "answer": "<The answer to the Your first question>", "source": "<Single numeric source ID backing the answer for Your first question>"},
+            {"question": "<Your second question>", "answer": "<The answer to the Your second question>", "source": "<Single numeric Source ID backing the answer for Your second question>"}
+        ],
+    "claim_veracity": {
+        "Supported": "<Likert-scale rating of how much You agree with the 'Supported' veracity classification>",
+        "Refuted": "<Likert-scale rating of how much You agree with the 'Refuted' veracity classification>",
+        "Not Enough Evidence": "<Likert-scale rating of how much You agree with the 'Not Enough Evidence' label>",
+        "Conflicting Evidence/Cherrypicking": "<Likert-scale rating of how much You agree with the 'Conflicting Evidence/Cherrypicking' label>"
+    }
+}
+```"""
 
     def __call__(
-        self, datapoint: Datapoint, retrieval_results: List[RetrievalResult], *args, **kwargs
+        self, datapoint: Datapoint, retrieval_result: RetrievalResult, *args, **kwargs
     ) -> EvidenceGenerationResult:
+        gpt_result = self.client(
+            system_prompt=self.format_system_prompt(retrieval_result), user_prompts=[datapoint.claim]
+        )
+        gpt_data = self.parse_json(gpt_result)
         return EvidenceGenerationResult(
-            evidences=[Evidence(answer="I don't know", url="")],
+            evidences=self.parse_evidence(gpt_data["questions"], retrieval_result),
             metadata={
-                "suggested_label": {
-                    "Supported": 0.5,
-                    "Refuted": 0.5,
-                    "Not Enough Info": 0.0,
-                    "Cherrypicking": 0.0,
-                },
+                "suggested_label": self.parse_label_probabilities(gpt_data["claim_veracity"]),
+                "llm_type": self.client.model,
+                "llm_output": gpt_data,
             },
         )
 
@@ -203,7 +296,7 @@ class DefaultClassifier(Classifier):
                 return ClassificationResult.from_dict({"probs": {suggested: 1.0}})
             if isinstance(suggested, dict):
                 return ClassificationResult.from_dict({"probs": suggested})
-            if isinstance(suggested, np.ndarray):
+            if isinstance(suggested, np.ndarray) or isinstance(suggested, list):
                 return ClassificationResult(probs=suggested)
             if isinstance(suggested, ClassificationResult):
                 return suggested
